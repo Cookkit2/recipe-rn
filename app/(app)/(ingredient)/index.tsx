3df19ad8f2892e787a2cc6fcb34b7f77c275d01f
BUF-Studio <bufstudio2020@gmail.com>
2025-09-06 14:17:34 +0800

    fix: rebase into main error

--- app/(app)/(ingredient)/index.tsx
+++ app/(app)/(ingredient)/index.tsx
@@ -1,20 +1,20 @@
 import { FlatList, View } from "react-native";
 import { H1, H4 } from "~/components/ui/typography";
 import { dummyPantryItems } from "~/data/dummy-data";
-import { IngredientItemCard } from "~/components/Ingredient/IngredientItemCard";
+import { IngredientItemCard } from "~/components/Pantry/IngredientItemCard";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { ThemeToggle } from "~/components/ThemeToggle";
-import ToggleButtonGroup from "~/components/Pantry/ToggleButtonGroup";
+import IngredientCategoryButtonGroup from "~/components/Pantry/IngredientCategoryButtonGroup";
 import MenuDropdown from "~/components/Pantry/MenuDropdown";
 import AddPantryItemModal from "~/components/Pantry/AddPantryItemModal";
-import useItemTypeStore from "~/store/type-store";
+import { usePantryStore } from "~/store/PantryContext";
 import RecipeButton from "~/components/Pantry/RecipeButton";
 
 export default function Screen() {
   const { bottom: pb, top: pt } = useSafeAreaInsets();
   const pantryItems = dummyPantryItems;
 
-  const { selectedItemType } = useItemTypeStore();
+  const { selectedItemType } = usePantryStore();
 
   const filteredItems = pantryItems.filter((item) => {
     if (selectedItemType === "all") return true;
@@ -30,7 +30,7 @@ export default function Screen() {
         <MenuDropdown />
         <ThemeToggle />
       </View>
-      <ToggleButtonGroup />
+      <IngredientCategoryButtonGroup />
       <View className="p-3">
         <FlatList
           numColumns={2}
@@ -38,8 +38,8 @@ export default function Screen() {
           showsVerticalScrollIndicator={false}
           data={filteredItems}
           keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item, index }) => (
-            <IngredientItemCard key={item.id} item={item} index={index} />
+          renderItem={({ item }) => (
+            <IngredientItemCard key={item.id} item={item} />
           )}
           ListEmptyComponent={
             <View className="py-16 items-center justify-center">
